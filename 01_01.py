b53470bd1d89e2790fd37101c52fd1af944e61fc
Natbud <96924057+Natbud@users.noreply.github.com>
2022-12-04 16:08:17 +0000

    01_01 completed

---
+++ 01_01.py
@@ -0,0 +1,17 @@
+filepath = "01_01_Data.txt"
+
+# read split whole file by paragraph (each list element is now a paragraph:
+with open(filepath) as f:
+    paragraphs = f.read().split("\n\n")
+
+all_elf_totals = []
+
+for n, paragraph in enumerate(paragraphs[0: None]):  # enumerate returns both value itself and also position.
+    elf_total = 0
+
+    for number in paragraph.split("\n"):
+        if number != '':
+            elf_total = elf_total + int(number)
+    all_elf_totals.append(int(elf_total))
+
+print("Elf with biggest total:", (max(all_elf_totals)))
