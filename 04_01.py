59de4ac6240d3101d0fa3fce69015e5917dcd090
Natbud <96924057+Natbud@users.noreply.github.com>
2022-12-07 20:37:46 +0000

    modified:   04_01.py 	new file:   04_01_Data_Chris.txt

--- 04_01.py
+++ 04_01.py
@@ -4,33 +4,30 @@ with open(thefilepath) as f:
     file_list = f.readlines()
     file_list = [line.strip() for line in file_list] #strip /n
 
-print(file_list)
+#print(file_list)
 
 found_lines = 0 
 
-for line in file_list:
-    
-    
-    if line.split("-", 1)[0] <= line.split(",")[1].split("-", 2)[0] and line.split("-", 1)[1].split(",")[0] >= line.split("-", 2)[2]:
+for line in file_list:   
+    if int(line.split("-", 1)[0]) <= int(line.split(",")[1].split("-", 2)[0]) and int(line.split("-", 1)[1].split(",")[0]) >= int(line.split("-", 2)[2]):
         print("Left digits fully contain right digits:", line)
         found_lines += 1
 
-    #check for digits same on either side of comma and move to next for iteration if so.
-    if line.split(",")[0] == line.split(",")[1]:
-            #print("SAME BOTH SIDES:",line)
-            continue   
-
-    if line.split("-", 1)[0] >= line.split(",")[1].split("-", 2)[0] and line.split("-", 1)[1].split(",")[0] <= line.split("-", 2)[2]:
+        #check for digits same on either side of comma and move to next for iteration if so.
+        if line.split(",")[0] == line.split(",")[1]:
+                #print("SAME BOTH SIDES:",line)
+                continue   
+    if int(line.split("-", 1)[0]) >= int(line.split(",")[1].split("-", 2)[0]) and int(line.split("-", 2)[2]) >= int(line.split("-", 1)[1].split(",")[0]):
         print("Right digits fully contain Left digits:", line)
-        found_lines += 1  
+        found_lines +=1
 
 # Remove duplicates and print foundlines:    
 print("total encapsulations found:", found_lines)
 
 # NOT WORKING VALUE IS TOO HIGH!!!  
 
-    #TESTING THE LINE SPLITS (WORKS FOR MULTIPLE DIGIT NUMBERS):
-    #print("first digits:",line.split("-", 1)[0])
-    #print("second digits:", line.split("-", 1)[1].split(",")[0])
-    #print("third digits:", line.split(",")[1].split("-", 2)[0])
-    #print("fourth digits:", line.split("-", 2)[2])
+#TESTING THE LINE SPLITS (WORKS FOR MULTIPLE DIGIT NUMBERS):
+#print("first digits:",line.split("-", 1)[0])
+#print("second digits:", line.split("-", 1)[1].split(",")[0])
+#print("third digits:", line.split(",")[1].split("-", 2)[0])
+#print("fourth digits:", line.split("-", 2)[2])
