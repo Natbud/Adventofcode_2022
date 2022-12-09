5afe2f3e80881e1c197c8eba2516320f881c57ad
Natbud <96924057+Natbud@users.noreply.github.com>
2022-12-09 16:48:31 +0000

    new file:   05_01.py 	new file:   05_01_Data.txt 	new file:   05_01_Test_Data.txt

---
+++ 05_01.py
@@ -0,0 +1,55 @@
+thefilepath = "05_01_Test_Data.txt"
+
+with open(thefilepath) as f:
+    file_list = f.readlines()
+    crate_list = [line.strip("\n") for line in file_list]
+    crate_list = list(filter(None, crate_list))
+    file_list = [line.strip() for line in file_list] #strips /n and whitespace
+
+print("stripped file_list", file_list)
+print("crate list:", crate_list)
+stacks_quantity = 0
+
+#find number of stacks:
+for i, line in enumerate(file_list):
+    #print("checking line:", line)
+    if line[0] == '1':
+        stacks_quantity = int(line[-1])
+        print("there are ", stacks_quantity, " stacks in this data")
+        break
+
+# Create correct no of stacks (just list objects):
+stacks_dict = {}
+
+for x in range (0,stacks_quantity):
+    stacks_dict[str(x+1)] = []
+
+
+
+#Add crate stacks to dictionary:
+for line in (crate_list):
+    if not line[1] == "1" and not line[0:4] == "move":
+        print("crate_list crates only:", line)
+        if not line[0:2] == "  ":
+            stacks_dict['1'].append(line[1])      
+    
+
+print(stacks_dict)
+
+#Now start moving crates:
+number_to_move = 0
+
+for line in (file_list):
+    if not line == '':
+        #print(line[0:4])
+        if line[0:4] =="move":
+            number_to_move = int(line.split(' ',5)[1])
+            move_from = line.split(' ', 5)[3]
+            move_to = line.split(' ', 5)[5]
+            #print("how many", number_to_move, "from where:", move_from, "moved to:", move_to)
+
+            for m in range(number_to_move):
+                #stacks_dict[move_from].pop()
+                print("item popped, dict update:", stacks_dict)
+
+
