6698d6a351582227e7ddc24cb2922c8eee1b0449
Natbud <96924057+Natbud@users.noreply.github.com>
2022-12-29 17:27:27 +0000

    09_01_worksfortestdataonly

--- 09_01.py
+++ 09_01.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-thefilepath = "09_01_Test_Data_Nath.txt"
+thefilepath = "09_01_Data.txt"
 
 with open(thefilepath) as f:
     file_list = f.readlines()
     file_list = [line.strip() for line in file_list] #strip /n
 
-print(file_list)
+#print(file_list)
 
 x_head = 0
 y_head = 0
@@ -20,7 +20,7 @@ change = 1
 for m, move in enumerate(file_list):
     distance = int(move.split(' ')[1])
 
-    print("distance: ",distance)
+    #print("distance: ",distance)
 
     for c in range(1,distance+1):
         #check if overlapping before any moves happen:
@@ -41,7 +41,49 @@ for m, move in enumerate(file_list):
         if x_head == x_tail and y_head == y_tail:
             change = 0
         
-        # Now Move Tail:
+        #check if head is adjacent to tail in N S E W directions:
+        if (x_head == x_tail and y_head == y_tail+1) or (x_head == x_tail+1 and y_head == y_tail) or (x_head == x_tail and y_head == y_tail-1) or (x_head == x_tail-1 and y_head == y_tail-1):
+            change = 0
+
+
+        #check if head is diagonally adjacent to tail:
+        if (x_head == x_tail+1 and y_head == y_tail+1) or (x_head == x_tail-1 and y_head == y_tail+1) or (x_head == x_tail-1 and y_head == y_tail-1) or (x_head == x_tail+1 and y_head == y_tail-1):
+            change = 0
+        
+        #Now intercept requirement for a diagonal tail move:
+        #TOP RIGHT DIAGONAL TAIL MOVE:
+        if (x_head == x_tail+1 and y_head == y_tail+2) or (x_head == x_tail+2 and y_head == y_tail+1):
+            x_tail +=1
+            y_tail +=1
+
+            tail_coordinates.append([x_tail,y_tail])
+            continue
+        
+        #TOP LEFT DIAGONAL TAIL MOVE:
+        if (x_head == x_tail-1 and y_head == y_tail+2) or (x_head == x_tail-2 and y_head == y_tail+1):
+            x_tail -=1
+            y_tail +=1
+
+            tail_coordinates.append([x_tail,y_tail])
+            continue
+        
+        #BOTTOM LEFT DIAGONAL TAIL MOVE:
+        if (x_head == x_tail-2 and y_head == y_tail-1) or (x_head == x_tail-1 and y_head == y_tail-2):
+            x_tail -=1
+            y_tail -=1
+
+            tail_coordinates.append([x_tail,y_tail])
+            continue
+
+        #BOTTOM RIGHT DIAGONAL TAIL MOVE:
+        if (x_head == x_tail+1 and y_head == y_tail-2) or (x_head == x_tail+2 and y_head == y_tail-1):
+            x_tail +=1
+            y_tail -=1
+
+            tail_coordinates.append([x_tail,y_tail])
+            continue
+
+        # STRAIGHT MOVES OF TAIL:
         if move[0] == "R":
             x_tail +=change
         if move[0] == "L":
@@ -60,6 +102,7 @@ for i in tail_coordinates:
     if i not in res:
         res.append(i)
 
-print("tail_coordinates: ", tail_coordinates)
-print("tail_co-or dedup: ", res)
+#print("tail_coordinates: ", tail_coordinates)
+#print("tail_co-or dedup: ", res)
+print("number of co-ordinates visited by tail: ", len(res))
 exit()
