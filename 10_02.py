ad93cf04f0f49b6b42bed59b66a2823f7cf76828
Natbud <96924057+Natbud@users.noreply.github.com>
2023-01-05 14:58:49 +0000

    Update 10_02.py

--- 10_02.py
+++ 10_02.py
@@ -12,29 +12,47 @@ with open(thefilepath) as f:
 
 global_cycle = 0
 register_value_x = 1
-crt_grid = np.full((6, 40), ".")
+crt_grid = np.full((6, 39), ".")
+crt_pos = 0
+print_row = 0
 
-print (tab.tabulate(crt_grid))
+def niceprint_grid ():
+    print (tab.tabulate(crt_grid))
 
-
+    
 for a in file_list:
+
+    if crt_pos <40:
+        print_row = 0
+    if crt_pos > 39 and crt_pos<:  #CONTINUE WORKING FROM HERE!!
+        print_row = 0   
+
+
     global_cycle +=1
 
+
+
     #DRAW A PIXEL ON CRT:
+    crt_grid[print_row][crt_pos] = "#"
 
 
     #END OF CYCLE COMMAND EXECUTION:
     if a[0] == "noop":
+        crt_pos +=1
         continue
     if a[0] == "addx":
         global_cycle +=1 #additional cycle added for addx which takes 2 cycles to complete
         
-        #DRAW A PIXEL ON CRT        
+        #DRAW A PIXEL ON CRT     
+        crt_grid[print_row][crt_pos] = "#"
 
+
+        #End of Cycle Commands:
         register_value_x += int(a[1])
+        crt_pos +=1
+    
 
-
-
+niceprint_grid()
 #print("total number of cycles in data: ", global_cycle)
 
 
@@ -42,3 +60,27 @@ for a in file_list:
 
 
 
+#SPARE BAD CODE:
+
+"""
+
+    if print_row == 0:
+        p = 0
+        q = 39
+    if print_row == 1:
+        p = 40
+        q = 79
+    if print_row == 2:
+        p = 80
+        q = 119
+    if print_row == 3:
+        p = 120
+        q = 159
+    if print_row == 4:
+        p = 160
+        q = 199
+    if print_row == 5:
+        p = 200
+        q = 239 
+
+"""
