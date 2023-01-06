e3536ce787468b9d0e9773de41e8c6dd482a760f
Natbud <96924057+Natbud@users.noreply.github.com>
2023-01-06 09:44:46 +0000

    Update 10_02.py

--- 10_02.py
+++ 10_02.py
@@ -2,7 +2,7 @@ import numpy as np
 import tabulate as tab
 import os
 
-thefilepath = "10_01_Test_Data.txt"
+thefilepath = "10_01_Data.txt"
 
 with open(thefilepath) as f:
     file_list = f.readlines()
@@ -18,6 +18,7 @@ crt_pos = 0  # NEED TO NOT USE THIS FOR PRINTING - NEED ANOTHER VALUE THAT RESET
 print_pos = 0
 print_row = 0
 noop_no = 0
+sprite_pos = 1
 
 def niceprint_grid ():
     print (tab.tabulate(crt_grid))
@@ -26,9 +27,15 @@ def exit_drawgrid():
     niceprint_grid()
     exit()
 
-def draw_pixel():
+def draw_pixel_hash():
     #DRAW A PIXEL ON CRT:
     crt_grid[print_row][print_pos] = "#"
+    #os.system('cls')
+    #niceprint_grid()
+
+def draw_pixel_dot():
+    #DRAW A PIXEL ON CRT:
+    crt_grid[print_row][print_pos] = " "
     #os.system('cls')
     #niceprint_grid()
 
@@ -57,10 +64,13 @@ for a in file_list:
 
     printpos_reset()
 
-    draw_pixel()
+    if print_pos == sprite_pos or print_pos == sprite_pos-1 or print_pos == sprite_pos+1:
+        draw_pixel_hash()
+    else:
+        draw_pixel_dot()
 
 
-    #END OF CYCLE COMMAND EXECUTION:
+    #NOOP OR ADDX:
     if a[0] == "noop":
         continue
     if a[0] == "addx":
@@ -73,61 +83,14 @@ for a in file_list:
 
         printpos_reset()
 
-        draw_pixel()
+        if print_pos == sprite_pos or print_pos == sprite_pos-1 or print_pos == sprite_pos+1:
+            draw_pixel_hash()
+        else:
+            draw_pixel_dot()
 
-        #End of Cycle Commands:
+        #UPDATE REGISTER:
         register_value_x += int(a[1])
+        sprite_pos = register_value_x
 
 niceprint_grid()
 print("total number of cycles in data: ", global_cycle)
-
-
-
-
-
-
-#SPARE BAD CODE:
-
-"""
-
-    if print_row == 0:
-        p = 0
-        q = 39
-    if print_row == 1:
-        p = 40
-        q = 79
-    if print_row == 2:
-        p = 80
-        q = 119
-    if print_row == 3:
-        p = 120
-        q = 159
-    if print_row == 4:
-        p = 160
-        q = 199
-    if print_row == 5:
-        p = 200
-        q = 239 
-
-"""
-
-"""
-        if crt_pos <40:
-            print_row = 0
-        if crt_pos > 39 and crt_pos < 80:
-            print_row = 1
-        if crt_pos > 79 and crt_pos < 120:
-            print_row = 2
-        if crt_pos > 119 and crt_pos < 160:
-            print_row = 3        
-        if crt_pos > 159 and crt_pos < 200:
-            print_row = 4
-        if crt_pos > 199 and crt_pos < 240:
-            print_row = 5
-"""
-
-"""
-    #IF NEWLINE then reset 'print_pos' to ZERO
-    if crt_pos == 40 or crt_pos == 80 or crt_pos == 120 or crt_pos == 160 or crt_pos == 200 or crt_pos == 240:
-        print_pos = 0
-"""
