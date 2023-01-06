eb0cd55561010fb478be7c3cd4d134dc1075ed47
Natbud <96924057+Natbud@users.noreply.github.com>
2023-01-06 08:55:08 +0000

    Update 10_02.py

--- 10_02.py
+++ 10_02.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tabulate as tab
+import os
 
 thefilepath = "10_01_Test_Data.txt"
 
@@ -14,7 +15,9 @@ global_cycle = 0
 register_value_x = 1
 crt_grid = np.full((6, 40), ".")
 crt_pos = 0  # NEED TO NOT USE THIS FOR PRINTING - NEED ANOTHER VALUE THAT RESETS TO ZERO EACH LINE
+print_pos = 0
 print_row = 0
+noop_no = 0
 
 def niceprint_grid ():
     print (tab.tabulate(crt_grid))
@@ -23,48 +26,65 @@ def exit_drawgrid():
     niceprint_grid()
     exit()
 
+def draw_pixel():
+    #DRAW A PIXEL ON CRT:
+    crt_grid[print_row][print_pos] = "#"
+    #os.system('cls')
+    #niceprint_grid()
+
+def newline_check():
+    global print_row
+    if print_pos == 40 or print_pos == 80 or print_pos == 120 or print_pos == 160 or print_pos == 200:
+        print_row +=1
+
+def printpos_reset():
+    global crt_pos
+    global print_pos
+    if crt_pos == 40 or crt_pos == 80 or crt_pos == 120 or crt_pos == 160 or crt_pos == 200 or crt_pos == 240:
+        print_pos = 0
+
     
 for a in file_list:
 
-    if crt_pos <40:
-        print_row = 0
-    if crt_pos > 39 and crt_pos < 80:
-        print_row = 1
-    if crt_pos > 79 and crt_pos < 120:
-        print_row = 2
-    if crt_pos > 119 and crt_pos < 160:
-        print_row = 3        
-    if crt_pos > 159 and crt_pos < 200:
-        print_row = 4
-    if crt_pos > 199 and crt_pos < 240:
-        print_row = 5
+    #OMIT ADVANCING PRINTPOS FIRST TIME ONLY
+    if crt_pos > 0:
+        print_pos +=1
+        crt_pos +=1 
 
     global_cycle +=1
 
-    #DRAW A PIXEL ON CRT:
-    crt_grid[print_row][crt_pos] = "#"
+    newline_check()
+
+    printpos_reset()
+
+    draw_pixel()
+
 
     #END OF CYCLE COMMAND EXECUTION:
     if a[0] == "noop":
-        crt_pos +=1
         continue
     if a[0] == "addx":
-        global_cycle +=1 #additional cycle added for addx which takes 2 cycles to complete
-        crt_pos +=1 # NEEDS TO HAPPEN BEFORE DRAWING HAPPENS.
+        #ADVANCE CYCLE:
+        global_cycle +=1
+        crt_pos +=1 
+        print_pos +=1
 
-        #DRAW A PIXEL ON CRT     
-        crt_grid[print_row][crt_pos] = "#"
+        newline_check()
+
+        printpos_reset()
+
+        draw_pixel()
 
         #End of Cycle Commands:
         register_value_x += int(a[1])
 
-    if crt_pos == 47:
-        exit_drawgrid()
+#    if crt_pos == 600:
+#        exit_drawgrid()
         
 
 
 niceprint_grid()
-#print("total number of cycles in data: ", global_cycle)
+print("total number of cycles in data: ", global_cycle)
 
 
 
@@ -94,4 +114,25 @@ niceprint_grid()
         p = 200
         q = 239 
 
+"""
+
+"""
+        if crt_pos <40:
+            print_row = 0
+        if crt_pos > 39 and crt_pos < 80:
+            print_row = 1
+        if crt_pos > 79 and crt_pos < 120:
+            print_row = 2
+        if crt_pos > 119 and crt_pos < 160:
+            print_row = 3        
+        if crt_pos > 159 and crt_pos < 200:
+            print_row = 4
+        if crt_pos > 199 and crt_pos < 240:
+            print_row = 5
+"""
+
+"""
+    #IF NEWLINE then reset 'print_pos' to ZERO
+    if crt_pos == 40 or crt_pos == 80 or crt_pos == 120 or crt_pos == 160 or crt_pos == 200 or crt_pos == 240:
+        print_pos = 0
 """
