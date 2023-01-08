49ba1df0ed395139df35f4033dd7405c1c6f4a9d
Natbud <96924057+Natbud@users.noreply.github.com>
2023-01-08 23:13:36 +0000

    Update 11_01.py

--- 11_01.py
+++ 11_01.py
@@ -1,11 +1,37 @@
+import numpy as np
+import tabulate as tab
 
 
-
-thefilepath = "10_01_Data.txt"
+thefilepath = "11_01_Data.txt"
 
 with open(thefilepath) as f:
     file_list = f.readlines()
+    
+with open(thefilepath) as f:
+    paragraphs = f.read().split("\n\n")
 
-#VARIABLES
-Monkey = 0
-items_current = []
+def niceprint_grid (grid):
+    print (tab.tabulate(grid))
+
+spacesplit_list = [line.strip() for line in file_list] #strip /n
+spacesplit_list = [line.split(' ') for line in spacesplit_list] #split at space character
+
+int_count = 0
+for line in spacesplit_list:
+    for word in line:
+        if word.isdigit():
+            int_count +=1
+
+print("file_list digit count:", int_count)
+
+
+
+monkey_item_grid = np.full((100,int(len(paragraphs))), "[]")
+
+niceprint_grid(monkey_item_grid)
+
+
+#monkey_item_grid = np.
+
+
+
