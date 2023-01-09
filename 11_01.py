3a54be6a53168fb8d29933271d19a84e68339633
Natbud <96924057+Natbud@users.noreply.github.com>
2023-01-09 07:43:46 +0000

    Update 11_01.py

--- 11_01.py
+++ 11_01.py
@@ -16,6 +16,7 @@ def niceprint_grid (grid):
 spacesplit_list = [line.strip() for line in file_list] #strip /n
 spacesplit_list = [line.split(' ') for line in spacesplit_list] #split at space character
 
+#count all 'items' in input file
 item_count = 0
 for line in spacesplit_list:
     if line[0] == "Starting":
@@ -24,14 +25,27 @@ for line in spacesplit_list:
             if word.isdigit():
                 item_count +=1
 
-print("file_list digit count:", item_count)
-
+#print("file_list total item count:", item_count)
 monkey_item_grid = np.full((int(len(paragraphs)),item_count), ".")
+
+# THIS BIT NOT WORKING!! NEED TO STRIP WHITESPACE FROM FONT OF PARAGRAPH LINES
+for p, paragraph in enumerate(paragraphs):
+    spacesplit_paragraph = paragraph.split("\n")
+    spacesplit_paragraph = [line.strip() for line in spacesplit_paragraph]
+    spacesplit_paragraph = [line.split(' ') for line in spacesplit_paragraph]
+    for line in spacesplit_paragraph:
+        if line[0] == "Starting":
+            add_count = 0
+            for word in line:
+                word = word.strip(',')
+                if word.isdigit():
+                    monkey_item_grid[p][add_count] = int(word)
+                    add_count +=1
 
 niceprint_grid(monkey_item_grid)
 
 
-#monkey_item_grid = np.
+
 
 
 
