a29fbf7cdffc218c91a10f6b22353befe2dc315c
Natbud <96924057+Natbud@users.noreply.github.com>
2023-01-10 22:38:37 +0000

    Update 11_01.py

--- 11_01.py
+++ 11_01.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tabulate as tab
+import math
 
 
 thefilepath = "11_01_Test_Data.txt"
@@ -17,6 +18,7 @@ spacesplit_list = [line.strip() for line in file_list] #strip /n
 spacesplit_list = [line.split(' ') for line in spacesplit_list] #split at space character
 
 operators = []
+maths_signs = []
 divisibles = []
 true_throws = []
 false_throws = []
@@ -31,7 +33,8 @@ for line in spacesplit_list:
                 item_count +=1
 
 #print("file_list total item count:", item_count)
-monkey_item_grid = np.full((int(len(paragraphs)),item_count), "  ")
+monkey_item_grid = np.full((int(len(paragraphs)),item_count), "  ", dtype='O')
+
 
 #Add intial items to each monkey in grid
 for p, paragraph in enumerate(paragraphs):
@@ -54,6 +57,11 @@ for p, paragraph in enumerate(paragraphs):
                 operators.append(operator)
             else:
                 operators.append("old")
+
+            maths_sign = line[len(line) -2]
+            maths_signs.append(maths_sign)
+
+        # now get the + or * modifier value from Operation line
         
         #now get Test divisor:
         if line[0] == "Test:":
@@ -61,23 +69,67 @@ for p, paragraph in enumerate(paragraphs):
             divisibles.append(divisible)
         
         # get if true throw:
-        if line[0] == "If true:":
+        if line[1] == "true:":
             true_throw = line[len(line) -1]
             true_throws.append(true_throw)
 
         # get if false throw:
-        if line[0] == "If false:":
+        if line[1] == "false:":
             false_throw = line[len(line) -1]
             false_throws.append(false_throw)          
 
 
 #Display collected data so far:
-print("operators: ", operators)
-print("divisibles: ", divisibles)
-print("true throws: ", true_throws)
-print("false throws: ", false_throws)
+#print("operators: ", operators)
+#print("divisibles: ", divisibles)
+#print("true throws: ", true_throws)
+#print("false throws: ", false_throws)
 niceprint_grid(monkey_item_grid)
 
 
+
+
 #Now start main procedure:
-#for monkey, items in enumerate(monkey_item_grid):
+for monkey, items in enumerate(monkey_item_grid):
+    temp_items = []
+    for item in items:
+        if not item == "  ":
+            temp_items.append(item)
+    print("temp items: ", temp_items)
+    for temp_item in temp_items:
+        #Find Destination Monkey for throw:
+        #add clause for some that need addition not multiplication.
+        if operators[monkey] == "old":
+            if maths_signs[monkey] == "*":
+                increased_worry = int(temp_item) * int(temp_item)
+            else:
+                increased_worry = int(temp_item) + int(temp_item)
+        else:
+            if maths_signs[monkey] == "*":
+                increased_worry = int(temp_item) * int(operators[monkey])
+            else:
+                increased_worry = int(temp_item) + int(operators[monkey])
+
+        div3_worry = math.floor(increased_worry/3)
+        divided_test = div3_worry/int(divisibles[monkey])
+        if divided_test == int(divided_test):
+            destination_monkey = int(true_throws[monkey])
+        else:
+            destination_monkey = int(false_throws[monkey])
+        print("\n\ndestination monkey: ",destination_monkey)
+        
+        #Throw the monkey:
+        #Remove from current monkey:
+        monkey_item_grid[monkey][0] = "  "
+        monkey_item_grid[monkey] = np.roll(monkey_item_grid[monkey], -1)
+        #Add to destination monkey:
+        for d, dest_item in enumerate(monkey_item_grid[destination_monkey]):
+            if dest_item == "  ":
+                monkey_item_grid[destination_monkey][d] = div3_worry
+                print("div3_worry value:", div3_worry)
+                print("value at new destination: ", monkey_item_grid[destination_monkey][d])
+                break
+        
+niceprint_grid(monkey_item_grid)
+
+        
