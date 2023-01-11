4558596dcef85fe294412198fb99812a289269d2
Natbud <96924057+Natbud@users.noreply.github.com>
2023-01-11 23:31:44 +0000

    Update 12_01.py

--- 12_01.py
+++ 12_01.py
@@ -1,7 +1,17 @@
+import string
+import numpy as np
+
+
+
 thefilepath = "12_01_Test_Data.txt"
 
 with open(thefilepath) as f:
     file_list = f.readlines()
 
 
+
+
+
+alphabet = list(map(chr, range(97, 123)))
+print(alphabet)
 
